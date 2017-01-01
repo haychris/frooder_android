6da367f9a030b0f9e363c45dc2d0eb76914f827d
Christopher Hay <christopher.ray.hay@gmail.com>
2017-01-01 01:48:39 -0500

    Added default settings activity

--- app/src/main/java/neeraj/christopher/frooder/MainTabbedActivity.java
+++ app/src/main/java/neeraj/christopher/frooder/MainTabbedActivity.java
@@ -384,6 +384,8 @@ public class MainTabbedActivity extends AppCompatActivity implements GoogleApiCl
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
